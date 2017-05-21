8b178e820c69add67f915f69ae8872100afba02c
Vyacheslav <voronenko@gmail.com>
2017-05-21 22:39:40 +0200

    Develop (#1)
    * fix(travis tests)

--- tests/build.sh
+++ tests/build.sh
@@ -2,16 +2,16 @@
 
 set -e
 
-TRAVIS_REPO_SLUG=`git remote --verbose | grep origin | grep fetch | cut -f2 | cut -d':' -f2 | cut -d' ' -f1`
-TRAVIS_COMMIT=`git rev-parse HEAD`
+path=roles/softasap.sa-role-under-test
+rm -rf $path || true
+git clone https://github.com/${TRAVIS_REPO_SLUG}.git $path
+cd $path && git checkout ${TRAVIS_COMMIT} && cd ../../
 
-ansible-playbook build.yml -e role_github_repo=${TRAVIS_REPO_SLUG} -e role_github_commit=${TRAVIS_COMMIT}
-grep failed=0 build.out | grep unreachable=0 >/dev/null
+
+ansible-playbook build.yml -e role_github_repo=${TRAVIS_REPO_SLUG} -e role_github_commit=${TRAVIS_COMMIT} -vvvv
 
 if [ "$?" == "0" ]; then
     # if all looks good, give it a go
     echo "Build completed successfully. Starting the project..."
     ansible-container run -d --production
- else
-    cat build.out
- fi 
+fi
