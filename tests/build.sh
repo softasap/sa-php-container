2a49cc09aac58b4b52bc1f7e89c99262b580b482
Vyacheslav Voronenko <git@voronenko.info>
2017-05-21 21:51:26 +0200

    improve(tests) resolved steps for conductor image

--- tests/build.sh
+++ tests/build.sh
@@ -4,8 +4,8 @@ set -e
 
 path=roles/softasap.sa-role-under-test
 rm -rf $path || true
-git clone https://${TRAVIS_REPO_SLUG} $path
-cd $path && git checkout https://github.com/${TRAVIS_COMMIT} && cd ../../
+git clone https://github.com/${TRAVIS_REPO_SLUG}.git $path
+cd $path && git checkout ${TRAVIS_COMMIT} && cd ../../
 
 
 ansible-playbook build.yml -e role_github_repo=${TRAVIS_REPO_SLUG} -e role_github_commit=${TRAVIS_COMMIT}
