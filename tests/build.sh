0eef4092536a393d2abbe2c3feff679bb9c832aa
Vyacheslav Voronenko <git@voronenko.info>
2017-05-21 21:27:04 +0200

    improve(tests) resolved steps for conductor image

--- tests/build.sh
+++ tests/build.sh
@@ -5,6 +5,24 @@ set -e
 TRAVIS_REPO_SLUG=`git remote --verbose | grep origin | grep fetch | cut -f2 | cut -d':' -f2 | cut -d' ' -f1`
 TRAVIS_COMMIT=`git rev-parse HEAD`
 
+git config -f .projmodules --get-regexp '^submodule\..*\.path$' > tempfile
+
+while read -u 3 path_key path
+do
+    url_key=$(echo $path_key | sed 's/\.path/.url/')
+    url=$(git config -f .projmodules --get "$url_key")
+    rm -rf $path
+    git clone https://github.com/${TRAVIS_REPO_SLUG} $path
+    echo "$path has been initialized"
+    cd $path && git checkout ${TRAVIS_COMMIT} && cd ../../
+
+done 3<tempfile
+
+rm tempfile
+
+exit 1
+
+
 ansible-playbook build.yml -e role_github_repo=${TRAVIS_REPO_SLUG} -e role_github_commit=${TRAVIS_COMMIT}
 grep failed=0 build.out | grep unreachable=0 >/dev/null
 
