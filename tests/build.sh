300ef41ff971962df5a0f2ebc67b0491ab253106
Vyacheslav Voronenko <git@voronenko.info>
2017-05-21 22:19:41 +0200

    improve(tests) resolved steps for conductor image

--- tests/build.sh
+++ tests/build.sh
@@ -8,13 +8,10 @@ git clone https://github.com/${TRAVIS_REPO_SLUG}.git $path
 cd $path && git checkout ${TRAVIS_COMMIT} && cd ../../
 
 
-ansible-playbook build.yml -e role_github_repo=${TRAVIS_REPO_SLUG} -e role_github_commit=${TRAVIS_COMMIT}
-grep failed=0 build.out | grep unreachable=0 >/dev/null
+ansible-playbook build.yml -e role_github_repo=${TRAVIS_REPO_SLUG} -e role_github_commit=${TRAVIS_COMMIT} -vvvv
 
 if [ "$?" == "0" ]; then
     # if all looks good, give it a go
     echo "Build completed successfully. Starting the project..."
     ansible-container run -d --production
- else
-    cat build.out
- fi 
+fi
